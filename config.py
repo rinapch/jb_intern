d915e77f60faec01ec665e2ef3ec591e32da47dd
rinapch <kiravsworld@gmail.com>
2024-04-26 13:46:38 +0200

    also use a little smaller context size

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ TMP_KOTLIN_REPO_PATH = "/tmp/kotlin_repo"  # Temporary path for cloning
 OUTPUT_RAW_FILE = "kotlin_code_raw.jsonl"
 OUTPUT_READY_DATASET_PATH = "kotlin_prepared_dataset"
 
-MIN_CONTEXT_LENGTH = 50  # Number of words (or special characters) to consider as context for code completion
+MIN_CONTEXT_LENGTH = 35  # Number of words (or special characters) to consider as context for code completion
 EOS_TOKEN = "<|endoftext|>"
